e4d015cd9f3062bbc1ee7820794ffd78408e2b0b
unknown <israr.javaid@yahoo.com>
2019-06-13 14:35:03 +0500

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const forecast=require('./utils/forecast')
 
 const app= express()
 
+const port=process.env.PORT || 3000
+
 //define paths for express config
 const pathToDirectory=path.join(__dirname, '../public')
 const viewspath=path.join(__dirname, '../templates/views')
@@ -81,6 +83,6 @@ app.get('*', (req, res)=>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is running on 3000')
+app.listen(port, ()=>{
+    console.log('Server is running on ' + port)
 })
